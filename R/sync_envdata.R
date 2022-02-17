ea7f9ab6b486e655b37c82ac7c2ac3d741248ccd
Adam Wilson <adamw@srv-v15-25.cbls.ccr.buffalo.edu>
2022-02-16 21:35:32 -0500

    remove cached files

--- R/sync_envdata.R
+++ R/sync_envdata.R
@@ -2,11 +2,12 @@
 
 sync_envdata <- function(repo="AdamWilsonLab/emma_envdata",
                          path="data/envdata"){
+  file.remove(list.files(path,full=T)) # delete all old files
+  #file.path(path,"stable_data.gz.parquet"))
   if(!file.exists(path)) dir.create(path,recursive = T)
   piggyback::pb_download(repo = repo,
                          dest = path,overwrite = T,
                          show_progress = F)
-  file.remove(file.path(path,"stable_data.gz.parquet"))
   piggyback::pb_download(file="stable_data.gz.parquet",
                          repo = repo,
                          dest = path,overwrite = T,
