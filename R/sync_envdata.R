ee9c34a9d69f28ba3a24c32f3da0012618cfb235
Adam Wilson <adamw@srv-v15-25.cbls.ccr.buffalo.edu>
2022-02-16 21:18:58 -0500

    update download

--- R/sync_envdata.R
+++ R/sync_envdata.R
@@ -4,6 +4,12 @@ sync_envdata <- function(repo="AdamWilsonLab/emma_envdata",
                          path="data/envdata"){
   if(!file.exists(path)) dir.create(path,recursive = T)
   piggyback::pb_download(repo = repo,
-                         dest = path,overwrite = T)
-  return(path)
+                         dest = path,overwrite = T,
+                         show_progress = F)
+  piggyback::pb_download(file="stable_data.gz.parquet",
+                         repo = repo,
+                         dest = path,overwrite = T,
+                         show_progress = F)
+
+    return(path)
 }
