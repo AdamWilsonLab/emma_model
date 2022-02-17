af09bdebd2f33f6ff3aa090626d878d0356cf1af
Adam Wilson <adamw@srv-v15-25.cbls.ccr.buffalo.edu>
2022-02-16 20:44:23 -0500

    piggyback overwrite

--- R/sync_envdata.R
+++ R/sync_envdata.R
@@ -4,6 +4,6 @@ sync_envdata <- function(repo="AdamWilsonLab/emma_envdata",
                          path="data/envdata"){
   if(!file.exists(path)) dir.create(path,recursive = T)
   piggyback::pb_download(repo = repo,
-                         dest = path,overwrite = F)
+                         dest = path,overwrite = T)
   return(path)
 }
