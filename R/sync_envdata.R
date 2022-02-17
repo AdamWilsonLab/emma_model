c44c33643e333478a44c16d7e0251b15468ba154
Adam Wilson <adamw@srv-v15-25.cbls.ccr.buffalo.edu>
2022-02-16 21:29:16 -0500

    try deleting stable data first.

--- R/sync_envdata.R
+++ R/sync_envdata.R
@@ -6,6 +6,7 @@ sync_envdata <- function(repo="AdamWilsonLab/emma_envdata",
   piggyback::pb_download(repo = repo,
                          dest = path,overwrite = T,
                          show_progress = F)
+  file.remove(file.path(path,"stable_data.gz.parquet"))
   piggyback::pb_download(file="stable_data.gz.parquet",
                          repo = repo,
                          dest = path,overwrite = T,
