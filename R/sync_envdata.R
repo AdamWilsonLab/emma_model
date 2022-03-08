fb5b50f3e49c65f0369b0ac8ed5dc46c5a00970d
Adam Wilson <adamw@srv-v15-25.cbls.ccr.buffalo.edu>
2022-03-08 11:36:17 -0500

    update release tag

--- R/sync_envdata.R
+++ R/sync_envdata.R
@@ -7,10 +7,12 @@ if(delete_all_first)  file.remove(list.files(path,full=T)) # delete all old file
   #file.path(path,"stable_data.gz.parquet"))
   if(!file.exists(path)) dir.create(path,recursive = T)
   piggyback::pb_download(repo = repo,
+                         tag = "current",
                          dest = path,overwrite = T,
                          show_progress = F)
   piggyback::pb_download(file="stable_data.gz.parquet",
                          repo = repo,
+                         tag = "current",
                          dest = path,overwrite = T,
                          show_progress = F)
 
