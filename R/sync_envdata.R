df1684dfa49fdf21498c4915efbcfdfe0a295fd9
Adam Wilson <adamw@srv-v15-25.cbls.ccr.buffalo.edu>
2022-03-09 21:35:30 -0500

    delete data files before downloading and then try 10 times to get them all.

--- R/sync_envdata.R
+++ R/sync_envdata.R
@@ -4,22 +4,31 @@ sync_envdata <- function(repo="AdamWilsonLab/emma_envdata",
                          path="data/envdata",
                          delete_all_first=F)  {
 
-                           if(delete_all_first)  file.remove(list.files(path,full=T)) # delete all old files
+  if(delete_all_first)  file.remove(list.files(path,full=T)) # delete all old files
+
+  # if running on github - delete the data files and get them again
+  if(Sys.getenv("CI")!="") file.remove(list.files(path,full=T)) # delete all old files
 
   if(!file.exists(path)) dir.create(path,recursive = T)
 
-  piggyback::pb_download(repo = repo,
+  n=10 # repeat n times to get everything.
+
+    for(i in 1:n){
+    tryCatch(
+    piggyback::pb_download(repo = repo,
                          tag = "current",
                          dest = path,
                          overwrite = T,
                          show_progress = F)
-
-  piggyback::pb_download(file="stable_data.gz.parquet",
+    )
+  tryCatch(
+    piggyback::pb_download(file="stable_data.gz.parquet",
                          repo = repo,
                          tag = "current",
                          dest = path,
                          overwrite = T,
                          show_progress = F)
+    )}
 
     return(path)
 }
