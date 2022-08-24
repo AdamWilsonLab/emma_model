51ba726b4b61e99587a12ce674fb4aeeb982c3d2
jslingsby <jslingsby@gmail.com>
2022-08-24 13:59:09 +0200

    Tweaks to change date range, narrow in on a smaller extent, and set the sampling proportion to 1 so that I can run it on my local machine

--- _targets.R
+++ _targets.R
@@ -5,7 +5,7 @@ library(arrow)
 library(piggyback)
 library(plotly)
 library(leaflet)
-#remotes::install_github("ropensci/stantargets")
+# remotes::install_github("ropensci/stantargets")
 # if(!"basemapR" %in% rownames(installed.packages())){
 #   devtools::install_github('Chrisjb/basemapR')
 # }
@@ -23,21 +23,22 @@ tar_option_set(packages = c("piggyback","cmdstanr", "posterior", "bayesplot", "t
                             "doParallel","raster"),
                deployment="main")
 
-Sys.setenv(HOME="/home/rstudio")
-cmdstanr::set_cmdstan_path()#"/home/rstudio/.cmdstanr/cmdstan-2.28.1")
-#cmdstanr::check_cmdstan_toolchain()
-#cmdstanr::install_cmdstan()
+# Sys.setenv(HOME="/home/rstudio")
+# cmdstanr::set_cmdstan_path()#"/home/rstudio/.cmdstanr/cmdstan-2.28.1")
+# #cmdstanr::check_cmdstan_toolchain()
+# #cmdstanr::install_cmdstan()
 
 # tar_destroy(ask = F)
 
 # Testing and training time windows
-training_window=c("2000-01-01","2020-01-01")
-testing_window=c("2020-01-01","2022-01-01")
+training_window=c("2000-01-01","2014-07-01")
+testing_window=c("2014-06-01","2022-07-01")
 
 # decide sampling proportion
-total_fynbos_pixels=348911
-sample_proportion=round(18000/total_fynbos_pixels,2);sample_proportion
-#tar_option_set(debug = "spatial_outputs")
+# total_fynbos_pixels=348911
+# sample_proportion=round(18000/total_fynbos_pixels,2);sample_proportion
+sample_proportion=1
+##tar_option_set(debug = "spatial_outputs")
 
 ## Download the most recent data release
 list(
@@ -58,8 +59,9 @@ list(
              tidy_static_data(
                envdata_files,
                remnant_distance=2, #drop pixels within this distance of remnant edge (km)
+               region=c(xmin = 19.49103, xmax = 19.54062, ymin = -33.97324, ymax = -33.92123), #jonaskop xmin: 19.49103 ymin: -33.97324 xmax: 19.54062 ymax: -33.92123
                #region=c(xmin = 18.3, xmax = 19.3, ymin = -34.3, ymax = -33.3), #core
-               region=c(xmin = 0, xmax = 30, ymin = -35, ymax = -20), #core
+               #region=c(xmin = 0, xmax = 30, ymin = -35, ymax = -20), #cfr
                #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531), #peninsula
                sample_proportion= sample_proportion, long_pixels=long_pixels)),
   tar_target(envvars,c( #select and possibly rename envvars to be included in model
@@ -110,24 +112,24 @@ list(
     #format="parquet"
   ),
 
-   tar_target(model_results,
-              summarize_model_output(model_summary_postfire_season, stan_data, envdata)),
-   tar_target(model_prediction,
+  tar_target(model_results,
+             summarize_model_output(model_summary_postfire_season, stan_data, envdata)),
+  tar_target(model_prediction,
              summarize_predictions(model_results,stan_data,envdata)),
   tar_target(spatial_outputs,
              create_spatial_outputs(envdata, envvars, model_results, data_training)),
- # tar_target(release,
- #            release_posteriors(
- #              model_output,
- #              file="targets/objects/model_results",
- #              repo = "AdamWilsonLab/emma_model",
- #              tag = "current")),
-   tar_render(report, "index.Rmd"),
- tar_target(name = reports,
-            command = generate_reports(output_directory = "reports/",
-                                       temp_directory = "data/temp/",
-                                       report_location = "report_prototype.rmd",
-                                       model_results = model_results,
-                                       model_prediction = model_prediction,
-                                       spatial_outputs = spatial_outputs))
+  # tar_target(release,
+  #            release_posteriors(
+  #              model_output,
+  #              file="targets/objects/model_results",
+  #              repo = "AdamWilsonLab/emma_model",
+  #              tag = "current")),
+  tar_render(report, "index.Rmd"),
+  tar_target(name = reports,
+             command = generate_reports(output_directory = "reports/",
+                                        temp_directory = "data/temp/",
+                                        report_location = "report_prototype.rmd",
+                                        model_results = model_results,
+                                        model_prediction = model_prediction,
+                                        spatial_outputs = spatial_outputs))
 )
