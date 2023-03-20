b90268e180d564e8a3bd0b2667d6e5e519afb520
Brian <bmaitner@gmail.com>
2023-03-20 13:49:19 -0400

    debugging

--- _targets.R
+++ _targets.R
@@ -28,7 +28,7 @@ tar_option_set(packages = c("piggyback","cmdstanr", "posterior", "bayesplot", "t
                             "doParallel","raster"),
                deployment="main")
 
-Sys.setenv(HOME="/home/rstudio")
+#Sys.setenv(HOME="/home/rstudio")
 cmdstanr::set_cmdstan_path()#"/home/rstudio/.cmdstanr/cmdstan-2.28.1")
 #cmdstanr::check_cmdstan_toolchain()
 #cmdstanr::install_cmdstan()
@@ -337,20 +337,20 @@ list(
   #~~~~~~~~~~~^
 
   # Release outputs
-  #   tar_target(publish_model_parameters,
-  #              release_model_parameters(model_prediction = model_prediction,
-  #                                       temp_directory_output = "data/model_parameters",
-  #                                       output_tag = "model_parameters",
-  #                                       chunk_size = 200000,
-  #                                       sleep_time = 1)),
-  #
-  # tar_target(publish_model_predictions,
-  #            release_ndvi_predictions(predicted_data = predicted_data,
-  #                                     temp_directory_output = "data/predicted_data",
-  #                                     output_tag = "ndvi_predictions",
-  #                                     chunk_size = 200000,
-  #                                     sleep_time = 1)),
-  #
+    tar_target(publish_model_parameters,
+               release_model_parameters(model_prediction = model_prediction,
+                                        temp_directory_output = "data/model_parameters",
+                                        output_tag = "model_parameters",
+                                        chunk_size = 200000,
+                                        sleep_time = 1)),
+
+  tar_target(publish_model_predictions,
+             release_ndvi_predictions(predicted_data = predicted_data,
+                                      temp_directory_output = "data/predicted_data",
+                                      output_tag = "ndvi_predictions",
+                                      chunk_size = 200000,
+                                      sleep_time = 1)),
+
   # Summaries
 
     tar_target(model_results,
