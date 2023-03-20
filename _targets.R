6a4bcb8540cc8d47eac04ebed8025966fe629112
Brian <bmaitner@gmail.com>
2023-03-20 12:35:11 -0400

    code to release model parms

--- _targets.R
+++ _targets.R
@@ -26,11 +26,9 @@ options(clustermq.scheduler = "multicore")
 tar_option_set(packages = c("piggyback","cmdstanr", "posterior", "bayesplot", "tidyverse",
                             "stringr","knitr","sf","stars","units","arrow","lubridate","stantargets",
                             "doParallel","raster"),
-               #deployment="main"
-               deployment="worker")
+               deployment="main")
 
-#Sys.setenv(HOME="/home/rstudio")
-#cmdstanr::install_cmdstan()
+Sys.setenv(HOME="/home/rstudio")
 cmdstanr::set_cmdstan_path()#"/home/rstudio/.cmdstanr/cmdstan-2.28.1")
 #cmdstanr::check_cmdstan_toolchain()
 #cmdstanr::install_cmdstan()
@@ -38,44 +36,50 @@ cmdstanr::set_cmdstan_path()#"/home/rstudio/.cmdstanr/cmdstan-2.28.1")
 # tar_destroy(ask = F)
 
 # Testing and training time windows
-training_window=c("2010-01-01","2020-01-01")
-testing_window=c("2020-01-01","2022-01-01")
-#predicting_window=c("2010-01-01","2020-01-01") #need to revise the predicting code to make it more memory efficient
+# training_window=c("2000-01-01","2020-01-01")
+# testing_window=c("2020-01-01","2022-01-01")
+# predicting_window=c("2000-01-01","2020-01-01") #need to revise the predicting code to make it more memory efficient
 #predicting_window=c("2020-01-01", as.character(Sys.Date()))
-predicting_window=c("2020-01-01", "2023-01-01")
+
+#For dev
+training_window=c("2020-01-01","2022-01-01")
+testing_window=c("2022-01-01","2023-01-01")
+predicting_window=c("2022-01-01","2023-01-01") #need to revise the predicting code to make it more memory efficient
+
+
 
 ## Download the most recent data release
 list(
-#
-#   tar_age(name = envdata_files,
-#           command = robust_pb_download(file=NULL,
-#                                        repo="AdamWilsonLab/emma_envdata",
-#                                        dest="data/envdata/",
-#                                        tag="current",
-#                                        show_progress=F,
-#                                        overwrite=F),
-#           age = as.difftime(7, units = "days"),
-#           #age = as.difftime(12, units = "hours"),
-#           format = "file"
-#           ),
 
-tar_target(
-  envdata_files,
-  robust_pb_download(file=NULL,
-                     repo="AdamWilsonLab/emma_envdata",
-                     dest="data/envdata/",
-                     tag="current",
-                     show_progress=T,
-                     overwrite=F),
-  format="file"),
+  # tar_age(name = envdata_files,
+  #         command = robust_pb_download(file=NULL,
+  #                                      repo="AdamWilsonLab/emma_envdata",
+  #                                      dest="data/envdata/",
+  #                                      tag="current",
+  #                                      show_progress=F,
+  #                                      overwrite=F),
+  #         age = as.difftime(7, units = "days"),
+  #         #age = as.difftime(12, units = "hours"),
+  #         format = "file"
+  #         ),
+
+  tar_target(
+    envdata_files,
+    robust_pb_download(file=NULL,
+                       repo="AdamWilsonLab/emma_envdata",
+                       dest="data/envdata/",
+                       tag="current",
+                       show_progress=F,
+                       overwrite=T),
+    format="file"),
 
   tar_target(envdata,
              tidy_static_data(
                envdata_files,
                remnant_distance=2, #drop pixels within this distance of remnant edge (km)
-               region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
-               #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531), #peninsula
-               sample_proportion= .25)),
+               #region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
+               region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531), #peninsula
+               sample_proportion= .05)),
 
   tar_target(
     data_training,
@@ -211,8 +215,8 @@ tar_target(
                envdata = envdata_files,
                remnant_distance=2, #drop pixels within this distance of remnant edge (km)
                #region=c(xmin = 16, xmax = 28, ymin = -35, ymax = -28), #whole region
-               #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531),#peninsula
-               region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
+               region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531),#peninsula
+               #region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
                sample_proportion= 1)),
 
 
@@ -317,38 +321,48 @@ tar_target(
   # ),
 
 
-  # tar_stan_vb_rep_summary(
-  #   prior_predict_output,
-  #   stan_files = "firemodel_prior_predict.stan",
-  #   data = stan_data,
-  #   batches = 1,
-  #   quiet=T,
-  #   reps = 1,
-  #   combine=T,
-  #   pedantic=T,
-  #   force_recompile=F,
-  #   #    stdout = R.utils::nullfile(),
-  #   #    stderr = R.utils::nullfile(),
-  #   adapt_engaged=F,
-  #   eta=0.11,
-  #   iter = 100, #should be 1000 or more - 100 is just to run quickly
-  #   garbage_collection=T,
-  #   init=1,
-  #   tol_rel_obj = 0.001
-  # ),
+  tar_stan_vb_rep_summary(
+    prior_predict_output,
+    stan_files = "firemodel_prior_predict.stan",
+    data = stan_data,
+    batches = 1,
+    quiet=T,
+    reps = 1,
+    combine=T,
+    pedantic=T,
+    force_recompile=F,
+    #    stdout = R.utils::nullfile(),
+    #    stderr = R.utils::nullfile(),
+    adapt_engaged=F,
+    eta=0.11,
+    iter = 100, #should be 1000 or more - 100 is just to run quickly
+    garbage_collection=T,
+    init=1,
+    tol_rel_obj = 0.001
+  ),
 
 
 
   #~~~~~~~~~~~^
 
+  # Release outputs
+  tar_target(publish_model_parameters,
+             release_model_parameters(model_prediction = model_prediction,
+                                      temp_directory_output = "data/model_parameters",
+                                      output_tag = "model_parameters",
+                                      chunk_size = 200000,
+                                      sleep_time = 1)),
 
-  tar_target(model_results,
-             summarize_model_output(model_output, stan_data, envdata)),
-  tar_target(model_prediction,
-             summarize_predictions(model_results,stan_data,envdata)),
-  tar_target(spatial_outputs,
-             create_spatial_outputs(model_results,data_training,envdata))
-  #,tar_render(report, "index.Rmd")
+  # Summaries
+
+    tar_target(model_results,
+               summarize_model_output(model_output, stan_data, envdata)),
+    tar_target(model_prediction,
+               summarize_predictions(model_results,stan_data,envdata)),
+    tar_target(spatial_outputs,
+               create_spatial_outputs(model_results,data_training,envdata))
+    #,tar_render(report, "index.Rmd")
+
 )
 
 
