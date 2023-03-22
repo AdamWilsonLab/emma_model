5a7eba4fcacdaf2c4e91d2e193a5fe61d0d44b93
Brian <bmaitner@gmail.com>
2023-03-22 10:58:44 -0400

    sample proportion: 0.2 -> 0.3 prediction region: core -> whole fynbos

--- _targets.R
+++ _targets.R
@@ -79,7 +79,7 @@ list(
                remnant_distance=2, #drop pixels within this distance of remnant edge (km)
                region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
                #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531), #peninsula
-               sample_proportion= .2)),
+               sample_proportion= .3)),
 
   tar_target(
     data_training,
@@ -205,9 +205,9 @@ list(
              tidy_static_data(
                envdata = envdata_files,
                remnant_distance=2, #drop pixels within this distance of remnant edge (km)
-               #region=c(xmin = 16, xmax = 28, ymin = -35, ymax = -28), #whole region
+               region=c(xmin = 16, xmax = 28, ymin = -35, ymax = -28), #whole region
                #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531),#peninsula
-               region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
+               #region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
                sample_proportion= 1)),
 
 
