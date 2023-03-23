1c6fd01bfad42e743e1b334b2fffd8a25d2475d8
Brian <bmaitner@gmail.com>
2023-03-23 13:55:28 -0400

    sample proportion 30% -> 40%

--- _targets.R
+++ _targets.R
@@ -79,7 +79,7 @@ list(
                remnant_distance=2, #drop pixels within this distance of remnant edge (km)
                region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
                #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531), #peninsula
-               sample_proportion= .3)),
+               sample_proportion= .4)),
 
   tar_target(
     data_training,
