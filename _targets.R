ae8fcfe10c4bd3ca842bb9679bebcc2a6b3ef111
Brian <bmaitner@gmail.com>
2023-03-23 19:07:34 -0400

    core region -> whole region sample proportion 40% -> 70%

--- _targets.R
+++ _targets.R
@@ -77,9 +77,10 @@ list(
              tidy_static_data(
                envdata_files,
                remnant_distance=2, #drop pixels within this distance of remnant edge (km)
-               region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
+               region=c(xmin = 16, xmax = 28, ymin = -35, ymax = -28), #whole region
+               #region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
                #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531), #peninsula
-               sample_proportion= .4)),
+               sample_proportion= .7)),
 
   tar_target(
     data_training,
