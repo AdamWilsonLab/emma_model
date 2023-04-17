b991c58372f83cc89a6a47ec7f1dd5dae4f3de29
Brian <bmaitner@gmail.com>
2023-04-17 15:12:59 -0400

    more debug

--- slurm_sing2.sh
+++ slurm_sing2.sh
@@ -10,12 +10,18 @@
 #SBATCH  -C INTEL
 #SBATCH --time=1:00:00
 
+
   export PROJECT_FOLDER="/projects/academic/adamw/"
   export APPTAINER_CACHEDIR="/scratch/"$USER"/singularity"
   export SIF_PATH=$PROJECT_FOLDER"/users/"$USER"/singularity"
   export SIF_FILE="AdamWilsonLab-emma_docker-latest.sif"
 
+
+  mkdir -p "$APPTAINER_CACHEDIR/tmp"
+  mkdir -p "$APPTAINER_CACHEDIR/run"
+
   singularity run \
   --bind $PROJECT_FOLDER:$PROJECT_FOLDER \
-  $SIF_PATH/$SIF_FILE ./emma_model/run.sh
-  
+  --bind $APPTAINER_CACHEDIR/tmp:/tmp \
+  --bind $APPTAINER_CACHEDIR/run:/run \
+  $SIF_PATH/$SIF_FILE ./run.sh
