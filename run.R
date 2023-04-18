9fce0478a984a1816e7eb76a5a484fb96a233246
Brian <bmaitner@gmail.com>
2023-04-18 12:22:19 -0400

    attempting to deal with github auth

--- run.R
+++ run.R
@@ -5,6 +5,17 @@
 # See https://books.ropensci.org/targets/hpc.html
 # to learn about your options.
 
-targets::tar_make()
+
+# authorize github.  Note this assumes there is an environmental variable
+  # called 'GITHUB_PAT", which contains your PAT
+
+  gitcreds::gitcreds_approve(list(protocol = "https",
+                                  host = "github.com",
+                                  username = NULL,
+                                  password = Sys.getenv("GITHUB_PAT")))
+
+#Run targets
+
+  targets::tar_make()
 # targets::tar_make_clustermq(workers = 2) # nolint
 # targets::tar_make_future(workers = 2) # nolint
