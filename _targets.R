e583d152c9e21f560227bbe29d20dd0fdf5077b8
Brian <bmaitner@gmail.com>
2023-04-24 16:10:14 -0400

    predicting: core 0.5 -> 1

--- _targets.R
+++ _targets.R
@@ -211,7 +211,7 @@ list(
                #region=c(xmin = 16, xmax = 28, ymin = -35, ymax = -28), #whole region
                #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531),#peninsula
                region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
-               sample_proportion= 0.5)),
+               sample_proportion= 1)),
 
   tar_target(
     data_predicting,
