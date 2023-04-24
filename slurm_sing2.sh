1546a5355f5a333109b50d43483654b8261bebad
Brian <bmaitner@gmail.com>
2023-04-24 14:44:04 -0400

    trying to use another cachedir

---
+++ slurm_sing2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --cluster=faculty
+#SBATCH --qos=adamw
+#SBATCH --partition=adamw
+#SBATCH --job-name "targets run"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=120G
+#SBATCH  -C INTEL
+#SBATCH --time=240:00:00
+
+
+  export PROJECT_FOLDER="/projects/academic/adamw/"
+  export APPTAINER_CACHEDIR="/panasas/scratch/grp-adamw/brianmai/singularity"
+  export SIF_PATH=$PROJECT_FOLDER"/users/"$USER"/singularity"
+  export SIF_FILE="AdamWilsonLab-emma_docker-latest.sif"
+
+
+  mkdir -p "$APPTAINER_CACHEDIR/tmp"
+  mkdir -p "$APPTAINER_CACHEDIR/run"
+
+  singularity run \
+  --bind $PROJECT_FOLDER:$PROJECT_FOLDER \
+  --bind $APPTAINER_CACHEDIR/tmp:/tmp \
+  --bind $APPTAINER_CACHEDIR/run:/run \
+  $SIF_PATH/$SIF_FILE ./run.sh
