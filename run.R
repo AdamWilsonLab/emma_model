6e5ab3f253f0ad5670d0052cada8415cd18a0a21
Brian <bmaitner@gmail.com>
2023-05-02 09:45:47 -0400

    updated tar_make verbosity

--- run.R
+++ run.R
@@ -20,7 +20,7 @@
 
 # Run targets
 
-  targets::tar_make()
+  targets::tar_make(reporter = "verbose")
 # targets::tar_make_clustermq(workers = 2) # nolint
 # targets::tar_make_future(workers = 2) # nolint
 
