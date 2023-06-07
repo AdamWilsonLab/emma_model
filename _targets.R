58d4f9176292649c1461fea9ea520b0742a819e2
Brian <bmaitner@gmail.com>
2023-06-07 11:00:31 -0400

    commented out the model_prediction, which relies on mu which we're not currently outputting

--- _targets.R
+++ _targets.R
@@ -210,8 +210,8 @@ list(
                envdata = envdata_files,
                remnant_distance=2, #drop pixels within this distance of remnant edge (km)
                region=c(xmin = 16, xmax = 28, ymin = -35, ymax = -28), #whole region
-               #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531),#peninsula
                #region=c(xmin = 18, xmax = 19.5, ymin = -35, ymax = -33), #core
+               #region=c(xmin = 18.301425, xmax = 18.524242, ymin = -34.565951, ymax = -34.055531),#peninsula
                sample_proportion= 1)),
 
   tar_target(
@@ -365,8 +365,11 @@ list(
 
   tar_target(model_results,
              summarize_model_output(model_output, stan_data, envdata)),
-  tar_target(model_prediction,
-             summarize_predictions(model_results,stan_data,envdata)),
+
+  #commenting this out, as we're currently not including mu in model output
+    # tar_target(model_prediction,
+    #            summarize_predictions(model_results,stan_data,envdata)),
+
   tar_target(spatial_outputs,
              create_spatial_outputs(model_results,data_training,envdata))
   #,tar_render(report, "index.Rmd")
