ff77326f023271be8083954fc7c2f95ad5d4d297
Brian <bmaitner@gmail.com>
2023-07-25 11:15:39 -0400

    added option to turn parquet or raw file uploading on/off

--- R/release_stan_objects.R
+++ R/release_stan_objects.R
@@ -1,9 +1,7 @@
 #' @destription Code to release model objects as rds
 #' @author Brian S Maitner
-#' @note Most targets objects can be loaded just fine as RDS files.  For some reason, the stan files are having issues.
-#' So, I'm trying a work-around here.
-#' @note: this issue is because stantargets uses other options than rds for saving.
-#' @note Also, this is designed for targets objects that are tables/dataframes
+#' @note Most targets objects can be loaded just fine as RDS files.  The ones genreated by stantargets are more problematic because they rely on alternative storage formats (which may also be verison-specific)
+#' @note Also, this is designed for targets objects that are tables/dataframes and uploads both the model object and a parquet by default.
 
 release_stan_objects <- function(object_names = c("model_summary_postfire_season",
                                                   "model_w_pred_summary_postfire_season_predict"),
@@ -11,6 +9,8 @@ release_stan_objects <- function(object_names = c("model_summary_postfire_season
                                  max_attempts = 10,
                                  sleep_time = 10,
                                  temp_directory="data/temp/pb_upload",
+                                 parquet=TRUE,
+                                 raw=TRUE,
                                  ...
                                  ){
 
@@ -38,35 +38,44 @@ release_stan_objects <- function(object_names = c("model_summary_postfire_season
 
     for(i in 1:length(object_names)){
 
-      # tar_load(object_names[i])
-      #
-      # write_parquet(x = get(object_names[i]),
-      #               sink = file.path(temp_directory,
-      #                                paste(object_names[i],".gz.parquet", sep = "")),
-      #               compression = "gzip")
-      #
-      # rm(list = object_names[i])
-      #
-      # robust_pb_upload(file = file.path(temp_directory,
-      #                                   paste(object_names[i],
-      #                                         ".gz.parquet", sep = "")),
-      #           repo = "AdamWilsonLab/emma_model",
-      #           tag = tag,
-      #           max_attempts = max_attempts,
-      #           sleep_time = sleep_time,
-      #           temp_directory = temp_directory)
-#
-#       file.remove(file.path(temp_directory,
-#                             paste(object_names[i],".gz.parquet", sep = "")))
+      if(parquet){
+
+        tar_load(object_names[i])
+
+        write_parquet(x = get(object_names[i]),
+                      sink = file.path(temp_directory,
+                                       paste(object_names[i],".gz.parquet", sep = "")),
+                      compression = "gzip")
+
+        rm(list = object_names[i])
+
+        robust_pb_upload(file = file.path(temp_directory,
+                                          paste(object_names[i],
+                                                ".gz.parquet", sep = "")),
+                  repo = "AdamWilsonLab/emma_model",
+                  tag = tag,
+                  max_attempts = max_attempts,
+                  sleep_time = sleep_time,
+                  temp_directory = temp_directory)
+
+              file.remove(file.path(temp_directory,
+                                    paste(object_names[i],".gz.parquet", sep = "")))
+
+      }#if parquet = TRUE
+
 
 
       #Upload raw objects
+
+      if(raw){
       robust_pb_upload(file = file.path("_targets/objects/",object_names[i]),
                        repo = "AdamWilsonLab/emma_model",
                        tag = tag,
                        max_attempts = max_attempts,
                        sleep_time = sleep_time,
                        temp_directory = temp_directory)
+
+      }#if raw=TRUE
 
 
 
